5ea638d0e06513326f0500e1e49a0fa9955563ed
Daniel Benner <daniel@devbydaniel.com>
2024-12-02 23:20:19 +0100

    fix: re-add task create route

--- src/pages/api/tasks/index.ts
+++ src/pages/api/tasks/index.ts
@@ -1,5 +1,25 @@
 import type { APIRoute } from "astro";
+import { createTask } from "../../../lib/data/taskWarrior";
 
-export const GET: APIRoute = async () => {
-  return new Response("Hi from TaskNinja!", { status: 200 });
+export const POST: APIRoute = async ({ request }) => {
+  const data = await request.formData();
+  const form = Object.fromEntries(data.entries());
+  const description = form.description as string | undefined;
+  const project = form.project as string | undefined;
+  const scheduled = form.scheduled as string | undefined;
+  const due = form.due as string | undefined;
+  const tags = Object.entries(form)
+    .filter(([tag, value]) => tag.startsWith("tag-") && value === "on")
+    .map(([tag]) => tag.replace("tag-", ""));
+
+  if (!description) {
+    return new Response(
+      JSON.stringify({
+        message: "Missing required field description",
+      }),
+      { status: 400 },
+    );
+  }
+  await createTask({ description, project, scheduled, due, tags });
+  return new Response(null, { status: 201 });
 };
