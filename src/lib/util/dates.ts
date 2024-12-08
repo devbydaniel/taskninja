f2ceeadb407567ff4349e7fd2df90e3ec5a3d6aa
Daniel Benner <daniel@devbydaniel.com>
2024-12-08 19:01:56 +0100

    fix: parse dates based on utc

--- src/lib/util/dates.ts
+++ src/lib/util/dates.ts
@@ -6,27 +6,15 @@ export function parseDate(date: string): Date {
   const minutes = date.length > 11 ? parseInt(date.slice(11, 13), 10) : 0;
   const seconds = date.length > 13 ? parseInt(date.slice(13, 15), 10) : 0;
 
-  // Create date assuming it is in local time (German winter time) and adjust to UTC
-  const localDate = new Date(year, month, day, hours, minutes, seconds);
-  const utcDate = new Date(
-    localDate.getTime() - localDate.getTimezoneOffset() * 60000,
+  // Create a UTC Date object
+  const utcDate = new Date(Date.UTC(year, month, day, hours, minutes, seconds));
+
+  // Convert UTC to local time
+  const localDate = new Date(
+    utcDate.getTime() - utcDate.getTimezoneOffset() * 60000,
   );
 
-  return utcDate;
-}
-
-export function stringifyDate(date: Date): string {
-  // Convert from UTC to local time (German winter time) before formatting
-  const localDate = new Date(date.getTime() + date.getTimezoneOffset() * 60000);
-
-  const year = localDate.getFullYear();
-  const month = (localDate.getMonth() + 1).toString().padStart(2, "0");
-  const day = localDate.getDate().toString().padStart(2, "0");
-  const hours = localDate.getHours().toString().padStart(2, "0");
-  const minutes = localDate.getMinutes().toString().padStart(2, "0");
-  const seconds = localDate.getSeconds().toString().padStart(2, "0");
-
-  return `${year}${month}${day}T${hours}${minutes}${seconds}Z`;
+  return localDate;
 }
 
 export function formatDate(
