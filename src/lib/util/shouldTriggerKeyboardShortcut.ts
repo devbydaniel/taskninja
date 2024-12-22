99377199ba2a00cb6da00c2e5106bac2f1827d95
Daniel Benner <daniel@devbydaniel.com>
2024-12-22 14:47:30 +0100

    feat: add shortcuts for reports

--- src/lib/util/shouldTriggerKeyboardShortcut.ts
+++ src/lib/util/shouldTriggerKeyboardShortcut.ts
@@ -1,6 +1,7 @@
 export default function shouldTriggerKeyboardShortcut(
   e: KeyboardEvent,
 ): boolean {
+  // do not trigger keyboard shortcuts while the user is typing something
   const target = e.target as HTMLElement;
   return !(
     target instanceof HTMLInputElement ||
